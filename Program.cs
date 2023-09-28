1719530065937c84a7acc23db8f5033348c5e9c9
Moisés Alves <moisesalves1@gmail.com>
2023-09-28 13:05:24 -0300

    Payload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using JwtAspNet.Models;
 using JwtAspNet.Services;
 using System.Security.Claims;
 
@@ -6,6 +7,17 @@ builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
-app.MapGet("/", (TokenService service, ClaimsPrincipal user) => service.Create());
+app.MapGet("/", (TokenService service) =>
+{
+    var user = new User(
+        1, 
+        "Moises Alves", 
+        "xyz@womi.com.br", 
+        "http://womi.com.br/xyz", 
+        "xyz", 
+        new[] { "student", "premium" });
+
+    return service.Create(user);
+});
 
 app.Run();
