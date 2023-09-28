2a61256148552c36e95f13c0cd3468935ff1fa1a
Moisés Alves <moisesalves1@gmail.com>
2023-09-28 12:36:48 -0300

    Iniciando o Token Service

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,10 +1,16 @@
-﻿namespace JwtAspNet.Services
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace JwtAspNet.Services
 {
     public class TokenService
     {
         public string Create()
         {
+            var handler = new JwtSecurityTokenHandler();
 
+            var token = handler.CreateToken();
+
+            return handler.WriteToken(token);
         }
     }
 }
